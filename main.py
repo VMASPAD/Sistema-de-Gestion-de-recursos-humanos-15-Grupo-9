4bb4fe5073cbc14a2f37be67c6b78be727bfc0a0
VMASPAD <tomaseavila@gmail.com>
2025-08-14 14:08:35 -0300

    Menu e inicio de operaciones CRUD

--- main.py
+++ main.py
@@ -6,3 +6,29 @@ licencias = []
 justificaciones = []
 #id, nombre y apellido, telefono, area, estado, fecha de ingreso, fecha de nacimiento
 empleados = []
+
+def Registrar():
+
+    return
+
+def Buscador():
+
+    return
+
+def Editar():
+    
+    return
+
+def Eliminar():
+    return
+def Menu():
+    print("="*26)
+    print('| 1. Registrar empleado  | ')
+    print('| 2. Obtener empleados   | ')
+    print('| 3. Editar empleado     | ')
+    print('| 4. Eliminar empleado   | ')
+    print('| 5. Salir               | ')
+    print("="*26)
+
+
+Menu()
