e3e8711ce8dd6fbd7a01e4dd7e6ec0b539b0a99d
VMASPAD <tomaseavila@gmail.com>
2025-08-14 13:51:01 -0300

    matrices creadas

---
+++ main.py
@@ -0,0 +1,8 @@
+#id, nombre, cantidad
+areas = []
+#id, id_empleado, fecha, id_justificacion
+licencias = []
+#id, justificacion
+justificaciones = []
+#id, nombre y apellido, telefono, area, estado, fecha de ingreso, fecha de nacimiento
+empleados = []
