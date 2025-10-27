7972496ea1f1589f38b6b55c5b6e0992e5b0db23
juanmestevezUADE <juanmestevez@uade.edu.ar>
2025-10-27 18:59:05 -0300

    Manejo de funciones: eliminado de empleados (completo)

--- CRUD/eliminar.py
+++ CRUD/eliminar.py
@@ -18,6 +18,7 @@ def eliminar_entidad_archivo(archivo, empleado, columna_id, columna):
     encontrado = False
     if archivo == archivos[0]:
         ent = "empleado"
+        cantidad = True
     elif archivo == archivos[1]:
         ent = "area"
     elif archivo == archivos[2]:
@@ -33,6 +34,8 @@ def eliminar_entidad_archivo(archivo, empleado, columna_id, columna):
             if empleado == id:
                 encontrado = True
                 datos[columna] = "Inactivo"
+                if cantidad:
+                    Modificar_cantidad_area(operacion= False, area=int(datos[4]))
                 nueva_linea = str(datos[0])
                 for dato in range(len(datos)):
                     if dato == 0:
@@ -68,5 +71,37 @@ def eliminar_entidad_archivo(archivo, empleado, columna_id, columna):
             print("Error al eliminar el archivo")
     return encontrado
 
+def Modificar_cantidad_area(operacion, area): #True : Sumar // False : Restar
+    try: 
+        with open(r"matrices/areas.txt", 'r', encoding='UTF-8') as arch, open(r'matrices/copia2.txt', 'w', encoding='UTF-8') as copia:
+            for linea in arch:
+                datos = linea.strip().split(";")
+                id_area = int(datos[0])
+                if id_area == area:
+                    if operacion:
+                        datos[2] = int(datos[2]) + 1
+                    else:
+                        datos[2] = int(datos[2]) - 1
+                    nueva_linea = str(datos[0])
+                    for i in range(len(datos)):
+                        if i == 0:
+                            continue
+                        else:
+                            nueva_linea += ";" + str(datos[i])
+                    nueva_linea += "\n"
+                    copia.write(nueva_linea)
+                else:
+                    copia.write(linea)
+    except FileNotFoundError:
+        print("No se encontró el archivo")
+    except OSError as error:
+        print("Ocurrió un error: ", error)
+    try: 
+        os.remove(r"matrices/areas.txt")
+        os.rename(r'matrices/copia2.txt', r"matrices/areas.txt")
+    except OSError:
+        print("No se pudo eliminar el archivo")
+                    
+
 if __name__ == "__main__":
-    eliminar_entidad_archivo(archivos[2], 11, 0, 4)
+    eliminar_entidad_archivo(archivos[0], 15, 0, 5)
