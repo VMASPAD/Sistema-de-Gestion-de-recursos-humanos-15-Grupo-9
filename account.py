4779e297d8a250c250193e07af35923b2b9648bd
fcordobadempf <fcordobadempf@uade.edu.ar>
2025-11-03 21:27:48 -0300

    sepuede abrir archivos de usuarios

--- account.py
+++ account.py
@@ -6,36 +6,43 @@ AZUL = '\033[94m'
 MAGENTA = '\033[95m'
 CIAN = '\033[96m'
 RESET = '\033[0m'
-
+import json
 
 #Funciones
-def userLog(usuarios, user=None, password=None):
-    usuarios_activos = {usuarios[i]["username"] for i in range(len(usuarios)) if usuarios[i]["estado"]=="Activo"}
-    if not user:
-        user = input(MAGENTA + "Ingrese su usuario: " + RESET).strip()
-    if not password:
-        password = input(MAGENTA + "Ingrese su contraseña: " + RESET).strip()
-    nivel_acceso = None
-    for i in range(len(usuarios)):
-        if user == usuarios[i]['username'] and user in usuarios_activos:
-            if password == usuarios [i]['password']:
-                nivel_acceso=usuarios[i]['nivel_acceso']
-            else: print(ROJO + "El usuario no coincide con la contraseña" + RESET)        
-    if not nivel_acceso:
-        print (ROJO + "No se encontró el usuario." + RESET)
-        return -1  
+def userLog(user=None, password=None):
+    try:
+        with open("dataset/usuarios.json", "r", encoding="UTF-8") as usu:
+                usuarios=json.load(usu)
+        usuarios_activos = {usuarios[i]["username"] for i in range(len(usuarios)) if usuarios[i]["estado"]=="Activo"}
+        if not user:
+            user = input(MAGENTA + "Ingrese su usuario: " + RESET).strip()
+        if not password:
+            password = input(MAGENTA + "Ingrese su contraseña: " + RESET).strip()
+        nivel_acceso = None
+        for i in range(len(usuarios)):
+            if user == usuarios[i]['username'] and user in usuarios_activos:
+                if password == usuarios [i]['password']:
+                    nivel_acceso=usuarios[i]['nivel_acceso']
+                else: print(ROJO + "El usuario no coincide con la contraseña" + RESET)     
+        if not nivel_acceso:
+            print (ROJO + "No se encontró el usuario." + RESET)
+            return -1  
 
-    if nivel_acceso=="admin": #user: juanp, pass: jp1234, DNI:40123456
-        print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
-        print()
+        if nivel_acceso=="admin": #user: juanp, pass: jp1234, DNI:40123456
+            print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
+            print()
+            
+            return 2
+        elif nivel_acceso=="user": #user: maria.g, pass: mg2024, DNI:38987456
+            print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
+            print()
+            
+            return 1
+        elif nivel_acceso == "guest": #user: lucasf, pass: lfpass, DNI:42111444
+            print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
+            print()
+            return 0
         
-        return 2
-    elif nivel_acceso=="user": #user: maria.g, pass: mg2024, DNI:38987456
-        print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
-        print()
-        
-        return 1
-    elif nivel_acceso == "guest": #user: lucasf, pass: lfpass, DNI:42111444
-        print(VERDE + "Bienvenido al sistema de gestion de recursos humanos" + RESET)
-        print()
-        return 0
+
+    except (OSError, FileNotFoundError) as error :
+        print("error", error)
--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def Menu():
             continue
         match opcion:
             case 1:
-                crear_usuario(usuarios)
+                crear_usuario()
             case 2:
-                nivel_acceso = userLog(usuarios)
+                nivel_acceso = userLog()
                 opcion = -1
                 if nivel_acceso == -1:
                     opcion = 6
@@ -191,18 +191,18 @@ def Menu():
                             tipo = Ingresar_Numero(MAGENTA + "Seleccione una opcion: " + RESET)
                             match tipo:
                                 case 1:
-                                    modulo_usuarios.buscar_usuarios(usuarios)
+                                    modulo_usuarios.buscar_usuarios()
                                 case 2:
                                     if nivel_acceso == 2:
-                                        crear_usuario(usuarios)
+                                        crear_usuario()
                                     else: print(ROJO + "No tiene permisos para realizar esta accion" + RESET)
                                 case 3:
                                     if nivel_acceso == 2:
-                                        modulo_usuarios.eliminar_usuario(usuarios)
+                                        modulo_usuarios.eliminar_usuario()
                                     else: print(ROJO + "No tiene permisos para realizar esta accion" + RESET)
                                 case 4:
                                     if nivel_acceso == 2:
-                                        modulo_usuarios.editar_usuario(usuarios)
+                                        modulo_usuarios.editar_usuario()
                                     else: print(ROJO + "No tiene permisos para realizar esta accion" + RESET)
                                 case 0:
                                     print(CIAN + "Volviendo al menu principal..." + RESET)
